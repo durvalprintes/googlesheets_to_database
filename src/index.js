77dc34248ce5d200fe1a13b5463b32ad4cf1c1b9
Durval Printes <durvalprintes@gmail.com>
2020-06-14 03:02:43 -0300

    Change pg-format to sql library, add a treatament for date type on query

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+const config = require("../config");
+
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 const { Client } = require("pg");
-const format = require("pg-format");
+const sql = require("sql");
 const date = require("moment");
-const config = require("../config");
 const file = require("fs");
 const mailer = require("nodemailer");
+
+sql.setDialect("postgres");
+const table = sql.define(config.table);
+
 var replacedByZero = "",
   days = [];
 
@@ -19,15 +24,51 @@ const sendMail = async () => {
   });
 };
 
+async function executeQuery(db, query) {
+  const { rows } = await db.query(query);
+  return rows;
+}
+
 const insertData = async (values) => {
-  const connectionString = config.connection;
-  let db;
+  let updates = [],
+    inserts = [];
+  const db = new Client({ connectionString: config.connection });
+  let result;
   try {
-    db = new Client({ connectionString });
     await db.connect();
     await db.query("BEGIN");
-    await db.query(format(config.delete, [...new Set(days)]));
-    await db.query(format(config.insert, values));
+    for (const value of values) {
+      result = await executeQuery(
+        db,
+        table
+          .select(table.id_registro)
+          .from(table)
+          .where(table.co_cnes.equals(value.co_cnes))
+          .and(table.dt_atendimento.equals(value.dt_atendimento))
+          .toQuery()
+      );
+      if (result[0] !== undefined) {
+        updates = updates.concat(
+          await executeQuery(
+            db,
+            table
+              .update(value)
+              .where(table.id_registro.equals(result[0].id_registro))
+              .returning(table.id_registro)
+              .toQuery()
+          )
+        );
+      } else {
+        inserts = inserts.concat(
+          await executeQuery(
+            db,
+            table.insert(value).returning(table.id_registro).toQuery()
+          )
+        );
+      }
+    }
+    console.log("Updates:", updates);
+    console.log("Inserts:", inserts);
     await db.query("COMMIT");
   } catch (error) {
     await db.query("ROLLBACK");
@@ -65,22 +106,26 @@ async function getData(doc) {
       ? date().subtract(2, "days").format("DD/MM/yyyy")
       : date().subtract(1, "days").format("DD/MM/yyyy");
   let registers = [],
-    fields;
+    fields = [],
+    c;
 
   for (let i = 0; i < rows.length && rows[i].DATA <= today; i++) {
     if (rows[i].DATA > lastWorkday) {
       days.push(rows[i].DATA);
-      fields = [doc.cnes];
+      fields.push([config.table.columns[(c = 1)], doc.cnes]);
       config.columns.forEach((column) => {
-        fields.push(
+        fields.push([
+          config.table.columns[++c],
           rows[i][column] === "" || rows[i][column] === undefined
             ? "0"
             : column !== "DATA"
             ? validData(sheet.title, column, rows[i][column])
-            : rows[i][column]
-        );
+            : new Date(
+                rows[i][column].replace(/(\d{2})\/(\d{2})\/(\d{4})/, "$2/$1/$3")
+              ),
+        ]);
       });
-      registers.push(fields);
+      registers.push(Object.fromEntries(fields));
     }
   }
 
@@ -101,18 +146,18 @@ const insertDataSheet = async () => {
     for (const sheet of config.sheets) {
       values = values.concat(await getData(sheet));
     }
-    values.sort((cnes_a, cnes_b) => cnes_a[0] - cnes_b[0]);
+    values.sort((value_a, value_b) => value_a.co_cnes - value_b.co_cnes);
     await insertData(values);
     if (replacedByZero != "") {
       Log(replacedByZero, "logData.txt");
-      console.log("Some fields was replaced by 0. See Data log for details.");
+      console.log("Some fields was replaced by 0. See data log for details.");
     }
     console.log("Status ok. Check the database.");
   } catch (error) {
-    console.log("Status failed. See error log for details.");
+    console.log("Status failed. See log for details.");
     Log(error.stack, "log.txt");
   } finally {
-    sendMail();
+    // sendMail();
   }
 };
 
